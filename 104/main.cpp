40913bdd3701e889fdf8f7b9ea75f5c5f225e23d
Jason An <anzhe0629@gmail.com>
2016-11-04 23:02:37 -0700

    add 104

--- 104/main.cpp
+++ 104/main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
 
+using namespace std;
+
+
+//Definition for a binary tree node.
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+
+    int dfs(TreeNode *root) {
+        if (root == NULL)
+            return 0;
+        else
+            return 1 + max(dfs(root->left), dfs(root->right));
+    }
+
+    int maxDepth(TreeNode* root) {
+
+        return dfs(root);
+    }
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    TreeNode a(3);
+
+    TreeNode *head = &a;
+
+    Solution ans;
+
+    cout << ans.maxDepth(head);
+
     return 0;
 }
