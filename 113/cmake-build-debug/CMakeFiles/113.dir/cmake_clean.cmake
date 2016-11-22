af3311c51726f64ea6a860b644bb3e077c77db64
Zhe <anzhe0629@gmail.com>
2016-11-22 12:33:01 -0800

    doing 113

---
+++ 113/cmake-build-debug/CMakeFiles/113.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/113.dir/main.cpp.o"
+  "113.pdb"
+  "113"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/113.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- 404/main.cpp
+++ 404/main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+//  Definition for a binary tree node.
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+
+        if (root == nullptr)
+            return 0;
+
+        if (root->left != nullptr && root->left->right == nullptr  && root->left->left == nullptr)
+            return root->left->val + sumOfLeftLeaves(root->right);
+
+        return sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
+    }
+
+};
+
+int main(){
+
+    TreeNode *head = new TreeNode(3);
+    head->left = new TreeNode(9);
+    head->right = new TreeNode(20);
+    head->right->left = new TreeNode(15);
+    head->right->right = new TreeNode(7);
+
+    Solution a;
+    cout << a.sumOfLeftLeaves(head);
     return 0;
+
 }
